459bf8541cc1d94688081674eb59843c1c2ce868
mohammad <zohrabi.dev@gmail.com>
2023-04-29 02:30:01 +0330

    translate to english

--- popup.js
+++ popup.js
@@ -1,14 +1,14 @@
 var toggleButton = document.getElementById('toggle');
 
 chrome.storage.sync.get('isEnabled', function (data) {
-  toggleButton.innerText = data.isEnabled ? 'غیر فعال' : 'فعال';
+  toggleButton.innerText = data.isEnabled ? 'OFF' : 'ON';
 });
 
 toggleButton.addEventListener('click', function () {
   chrome.storage.sync.get('isEnabled', function (data) {
     var isEnabled = !data.isEnabled;
     chrome.storage.sync.set({ isEnabled: isEnabled }, function () {
-      toggleButton.innerText = isEnabled ? 'غیر فعال' : 'فعال';
+      toggleButton.innerText = isEnabled ? 'OFF' : 'ON';
       chrome.runtime.sendMessage({ type: 'toggleBypass', isEnabled: isEnabled });
     });
   });
@@ -18,7 +18,7 @@ toggleButton.addEventListener('click', function () {
 chrome.storage.sync.get('isEnabled', function (data) {
   var isEnabled = data.isEnabled !== undefined ? data.isEnabled : true;
   chrome.storage.sync.set({ isEnabled: isEnabled }, function () {
-    toggleButton.innerText = isEnabled ? 'غیر فعال' : 'فعال';
+    toggleButton.innerText = isEnabled ? 'OFF' : 'ON';
     chrome.runtime.sendMessage({ type: 'toggleBypass', isEnabled: isEnabled });
   });
 });
